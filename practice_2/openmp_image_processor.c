ee54d620525a0b6cc6a9df4b476c2c968dbc3bc2
Santiago Mondragón Gómez <santiago.mondragon@mercadolibre.com.co>
2021-11-23 22:11:14 -0500

    works somewhat

--- practice_2/openmp_image_processor.c
+++ practice_2/openmp_image_processor.c
@@ -3,7 +3,6 @@
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "lib/stb_image_write.h"
 #include <omp.h>
-#define OMP_NUM_THREADS 6
 
 // function prototypes
 void apply_filter(unsigned char *image, int width, int height, int channels);
@@ -11,13 +10,14 @@ void apply_filter(unsigned char *image, int width, int height, int channels);
 int main(int argc, char const *argv[]) {
     const char *input_filename = argv[1];
     const char *output_filename = argv[2];
-    const char *num_of_threads = argv[3];
+    const char *threads_arg = argv[3];
 
     int width, height;
     int channels; // (like R, G, B and such)
+    int num_of_threads = atoi(threads_arg);
 
-    printf("%d\n", atoi(num_of_threads));
-    printf("%d", omp_get_num_threads());
+    omp_set_num_threads(num_of_threads);
+
     // load image
     unsigned char *image = stbi_load(input_filename, &width, &height, &channels, 0); // 0 is default to avoid forcing a specific channel number
     unsigned int image_size = width * height * channels;
@@ -47,33 +47,42 @@ int main(int argc, char const *argv[]) {
     return 0;
 }
 
-void apply_filter(unsigned char *image, int width, int height, int channels) {
+void apply_filter(unsigned char *image_start, int width, int height, int channels) {
     // color inversion filter
     // for b&w, it inverts luminosity
     // for b&w with alpha, transparency areas are inverted as well
+
     int image_size = width * height * channels;
 
+    if (channels < 2) {
+// B&W/G images
 #pragma omp parallel for
-    for (unsigned char *i = image; i < image + image_size; i += channels) {
+        for (unsigned char *current_image_idx = image_start; current_image_idx < image_start + image_size; current_image_idx += channels) {
 
-        if (channels <= 2) {
-            // B&W/G images
-            unsigned char *gray = i;
-
+            unsigned char *gray = current_image_idx;
             *gray = 255 - *gray;
         }
-        if (channels == 2) {
+    }
+    if (channels == 2) {
+#pragma omp parallel for
+        for (unsigned char *current_image_idx = image_start; current_image_idx < image_start + image_size; current_image_idx += channels) {
+
             // G, A images
-            unsigned char *alpha = (i + 1);
+            unsigned char *alpha = (current_image_idx + 1);
 
             //invert
             *alpha = 255 - *alpha;
-        } else if (channels == 3) {
+        }
+    }
+    if (channels == 3) {
+#pragma omp parallel for
+        for (unsigned char *current_image_idx = image_start; current_image_idx < image_start + image_size; current_image_idx += channels) {
+
             // R, G, B images
 
-            unsigned char *red = i;
-            unsigned char *green = (i + 1);
-            unsigned char *blue = (i + 2);
+            unsigned char *red = current_image_idx;
+            unsigned char *green = (current_image_idx + 1);
+            unsigned char *blue = (current_image_idx + 2);
 
             //swap colors around
             *red = 255 - *red;
